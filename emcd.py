2138c8b2c2a3449ca7f4ed248c15d7f22ff54d97
alxkvx <worksyn@gmail.com>
2020-03-07 02:26:00 +0300

    added last timestamp check

--- emcd.py
+++ emcd.py
@@ -1,14 +1,19 @@
 import urllib
 import json
+import time
 
 url = "https://api.emcd.io/v1/btc/income/8fbdd6e0-969d-47a6-808d-66635e50e81d"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 
 reward = data['income'][0]['income']
-hrate = data['income'][0]['total_hashrate']
+hrate  = data['income'][0]['total_hashrate']
+timest = data['income'][0]['timestamp']
 
-profit = reward/(float(hrate)/10**12)
-
-print ("Profit per 1 Th/Day: %.8f" % profit)
+diff = time.time() - timest
+if diff > 24*3600:
+    print "Last data is more than 24H old: %d H" % (int(diff)/3600)
+else:
+    profit = reward/(float(hrate)/10**12)
+    print ("Profit per 1 Th/Day: %.8f" % profit)
 
